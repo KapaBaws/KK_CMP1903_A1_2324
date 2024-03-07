0442c786ae7bdd4925fd485bf93b7717161fb9d9
KapaBaws <158062142+KapaBaws@users.noreply.github.com>
2024-03-07 15:06:44 +0000

    Update Die.cs

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,16 +8,16 @@ namespace CMP1903_A1_2324
 {
     internal class Die
     {
-        /*
-         * The Die class should contain one property to hold the current die value,
-         * and one method that rolls the die, returns and integer and takes no parameters.
-         */
+        public int DieValue { get; set; }
 
-        //Property
-
-
-        //Method
-
-
+        public static Random random = new Random();
+        
+        public int Roll()
+        {
+            DieValue = random.Next(1,7);
+            return DieValue;
+           
+        }
+        
     }
 }
