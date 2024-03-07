a9bb471510bd515f1355d54be330c5753da4a85e
KapaBaws <158062142+KapaBaws@users.noreply.github.com>
2024-03-07 15:15:20 +0000

    Update Die.cs

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -8,14 +8,14 @@ namespace CMP1903_A1_2324
 {
     internal class Die
     {
-        public int DieValue { get; set; }
+        public int DieValue { get; set; } // Allows for Die to be able to hold a value.
 
-        public static Random random = new Random();
+        public static Random random = new Random(); // Creates a static class which is able to generate random numbers.
         
-        public int Roll()
+        public int Roll() // A class which rolls the dice and gives it a value.
         {
-            DieValue = random.Next(1,7);
-            return DieValue;
+            DieValue = random.Next(1,7); // Generates a random number from 1-6 and assigns it to DieValue.
+            return DieValue; // Returns the value of the die.
            
         }
         
